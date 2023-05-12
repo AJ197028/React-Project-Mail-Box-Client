ec06acafe6111089eb425c7e4e88f99ea0b74c54
AJ197028 <aksjais20@gmail.com>
2023-05-12 06:50:14 +0530

    Login Page

--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -1,28 +1,76 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import { authActions } from "../Store";
 
 const Login = () => {
+  const [login, setLogin] = useState(false);
+
   const enteredMail = useRef();
   const enteredPassword = useRef();
   const enteredConfirmPassword = useRef();
 
+  let dispatch = useDispatch();
+  const history = useHistory();
+
+  const toggleLogin = () => {
+    setLogin(!login);
+  };
+
   const handleLogin = async (e) => {
     e.preventDefault();
-    if (
-      enteredMail.current.value.length > 0 &&
-      enteredPassword.current.value.length > 0 &&
-      enteredConfirmPassword.current.value.length > 0
-    ) {
+    if (!login) {
       if (
-        enteredPassword.current.value !== enteredConfirmPassword.current.value
+        enteredMail.current.value.length > 0 &&
+        enteredPassword.current.value.length > 0 &&
+        enteredConfirmPassword.current.value.length > 0
       ) {
-        alert("password and confirmPassword not matching");
+        if (
+          enteredPassword.current.value !== enteredConfirmPassword.current.value
+        ) {
+          alert("password and confirmPassword not matching");
+        } else {
+          try {
+            let responce = await fetch(
+              "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDWDs56jDC15FR_Jdlsh_OCOS4nc6_JuUA",
+              {
+                method: "POST",
+                body: JSON.stringify({
+                  email: enteredMail.current.value,
+                  password: enteredPassword.current.value,
+                  returnSecureToken: true,
+                }),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+            if (responce.ok) {
+              console.log("User has successfully signed up");
+              alert(`User has successfully signed up`);
+            } else {
+              alert("Authentication failed");
+              throw new Error("Sign up failed");
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        }
       } else {
+        alert("please fill all the data");
+      }
+    } else {
+      if (
+        enteredMail.current.value.length > 0 &&
+        enteredPassword.current.value.length > 0
+      ) {
         try {
-          let response = await fetch(
-            "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDWDs56jDC15FR_Jdlsh_OCOS4nc6_JuUA",
+          let responce = await fetch(
+            "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDWDs56jDC15FR_Jdlsh_OCOS4nc6_JuUA",
             {
               method: "POST",
               body: JSON.stringify({
@@ -35,26 +83,30 @@ const Login = () => {
               },
             }
           );
-          if (response.ok) {
-            console.log("User has successfully signed up");
-            alert(`User has successfully signed up`);
+          if (responce.ok) {
+            const data = await responce.json();
+            console.log(data.idToken);
+            dispatch(authActions.setToken(data.idToken));
+            localStorage.setItem("token", data.idToken);
+            console.log("User has successfully Log in");
+            alert(`User has successfully logged in`);
+            history.push("/profile");
           } else {
             alert("Authentication failed");
-            throw new Error("Sign up failed");
+            throw new Error("Log in failed");
           }
         } catch (error) {
           console.log(error);
         }
+      } else {
+        alert("please fill all the data");
       }
-    } else {
-      alert("please fill all the data");
     }
   };
-
   return (
     <div className="container w-25 my-3 border border-1 align-top bg-light rounded">
       <Form className="my-auto">
-        <h4 className="text-center py-4">Sign Up</h4>
+        <h4 className="text-center py-4">{!login ? "Sign Up" : "Log In"}</h4>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Control type="email" placeholder="Email" ref={enteredMail} />
         </Form.Group>
@@ -65,23 +117,30 @@ const Login = () => {
             ref={enteredPassword}
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
-          <Form.Control
-            type="password"
-            placeholder="Confirm Password"
-            ref={enteredConfirmPassword}
-          />
-        </Form.Group>
+        {!login && (
+          <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+            <Form.Control
+              type="password"
+              placeholder="Confirm Password"
+              ref={enteredConfirmPassword}
+            />
+          </Form.Group>
+        )}
         <Button
           variant="primary"
           className="w-100"
           type="submit"
           onClick={handleLogin}
         >
-          Sign Up
+          {!login ? "Sign Up" : "Log In"}
         </Button>
-        <Button variant="outline-success" className="my-2 w-100">
-          Have an account?Login
+        {login && <p className="text-center text-primary">Forget password?</p>}
+        <Button
+          variant="outline-success"
+          className="my-2 w-100"
+          onClick={toggleLogin}
+        >
+          {!login ? "Have an account?Login" : "Haven't Account?Sign Up"}
         </Button>
       </Form>
     </div>
